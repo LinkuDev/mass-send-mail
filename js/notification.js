0c6a1589401c27cf459148c7275ca86ddf2b47eb
Nguyen Xuan Linh <kiaisoft.linh.nguyen2@gmail.com>
2024-10-02 15:01:37 +0700

    a

--- js/notification.js
+++ js/notification.js
@@ -232,8 +232,8 @@ function sendEmailsInParallel(emails, logContent) {
   var totalEmails = emails.length;
   var completedEmails = 0;
   var hasCriticalError = false;
-  var delayBetweenRequests = 1500; // Giới hạn 1 request mỗi giây (1000ms = 1 giây)
-  var maxRequestsPerMinute = 40;
+  var delayBetweenRequests = 2500; // Giới hạn 1 request mỗi giây (1000ms = 1 giây)
+  var maxRequestsPerMinute = 30;
   var activePromises = 0;
   var currentIndex = 0;
 
